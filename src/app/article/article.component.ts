838bc6ef1c01e965bac910c37b1122bbb10788e3
CybeRooT-01 <birahim267@gmail.com>
2023-08-17 18:42:29 +0000

    changing structure

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -11,25 +11,7 @@ import { category } from '../interface/categories';
   styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent implements OnInit {
-  imageFile: any;
-  fournisseurs: Fournisseur[] = [];
-  searchTerm: string = '';
-  extension: string = '';
-  pathToImage: string = '';
-  imageEnBase64: string = '';
-  getSelectedCategory: string = '';
-  extensionsAutorised = ['jpg', 'jpeg', 'png', 'jfif'];
-  filteredFournisseurs: Fournisseur[] = [];
-  libelleValue: string = '';
-  libCode: string = '';
-  ref: string = 'REF-';
   categories: category[] = [];
-  selectedFournisseurs: number[] = [];
-  num: number = 1;
-  categoryId: number;
-  prix: number = 0;
-  stock: number = 0;
-  selectedFournisseursName: string[] = [];
 
   constructor(private categorieService: CategorieService) {}
   ngOnInit(): void {
@@ -37,106 +19,15 @@ export class ArticleComponent implements OnInit {
   }
   categoryservice: CategorieService = this.categorieService;
 
-  onFileChange(event: { target: { files: ArrayBuffer[] } }) {
-    console.log(this.libCode);
-    this.imageFile = event.target.files[0];
-    this.extension = this.imageFile.name.split('.')[1];
-    if (!this.extensionsAutorised.includes(this.extension)) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Vous devez choisir une image',
-      });
-      return;
-    }
-    const reader = new FileReader();
-    reader.onload = () => {
-      this.imageFile = reader.result as string;
-    };
-    reader.readAsDataURL(this.imageFile);
-  }
-  getValue() {
-    this.libCode =
-      this.libelleValue[0] + this.libelleValue[1] + this.libelleValue[2];
-  }
-
   chargerCategorie() {
     this.categoryservice.getCategories().subscribe((data) => {
       this.categories = data;
+      console.log(this.categories);
     });
   }
-  getAllFournisseur(){
-    this.categoryservice.getFournisseurs().subscribe((data) => {
-      this.fournisseurs = data;
-      console.log(this.fournisseurs);
-
-      const filteredFournisseurs = this.fournisseurs.filter((fournisseur) => {
-        return fournisseur.nom
-          .toLowerCase()
-          .includes(this.searchTerm.toLowerCase());
-      });
-      this.fournisseurs = filteredFournisseurs;
-      this.filteredFournisseurs = filteredFournisseurs;
-      if (this.searchTerm == '') {
-        this.fournisseurs = [];
-        this.filteredFournisseurs = [];
-      }
-      console.log(this.filteredFournisseurs);
-    });
-  }
-  selectFournisseur(fournisseur: {
-    id: number;
-    nom: string;
-    selected: boolean;
-  }) {
-    this.selectedFournisseurs.push(fournisseur.id);
-    this.selectedFournisseursName.push(fournisseur.nom);
-    console.log(this.selectedFournisseurs);
-    fournisseur.selected = true;
-    this.searchTerm = this.selectedFournisseursName.map((f) => f).join(', ');
-  }
-  deselectFournisseur(fournisseur: {
-    id: number;
-    selected: boolean;
-    nom: string;
-  }) {
-    this.selectedFournisseurs = this.selectedFournisseurs.filter(
-      (f) => f !== fournisseur.id
-    );
-    console.log(this.selectedFournisseurs);
-    fournisseur.selected = false;
-    this.selectedFournisseursName = this.selectedFournisseursName.filter(
-      (f) => f !== fournisseur.nom
-    );
-    this.searchTerm = this.selectedFournisseursName.map((f) => f).join(', ');
-  }
-  onInputChange() {
-    this.selectedFournisseurs = [];
-  }
-  updateref() {
-    const libCode = this.libCode.substring(0, 3);
-    let ref = 'REF-' + libCode.toUpperCase();
-    ref += '-' + this.getSelectedCategory.toUpperCase();
-    ref += '-' + this.num;
-    this.ref = ref;
-  }
-  onCategoryChange(event: { target: { options: { selectedIndex: number } } }) {
-    this.categoryId = event.target.options.selectedIndex;
-  }
-  ajouterArticle() {
-    const data = {
-      libelle: this.libelleValue,
-      reference: this.ref,
-      image: this.imageFile,
-      fournisseur: this.selectedFournisseurs,
-      categorie: this.categoryId,
-      prix: this.prix,
-      stock: this.stock,
-    };
+  ajouterArticle(data : any) {
     console.log(data);
-
     this.categoryservice.addArticle(data).subscribe((data) => {
-      this.num++;
       if (data) {
         Swal.fire('Good job!', 'Categorie Ajouté avec success', 'success');
       }
--- src/app/article/form/form.component.ts
+++ src/app/article/form/form.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
 import { CategorieService } from '../../services/categorie.service';
 import Swal from 'sweetalert2/dist/sweetalert2.js';
 import { Fournisseur } from 'src/app/interface/Fournisseurs';
+import { NgForm,FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -9,36 +10,26 @@ import { Fournisseur } from 'src/app/interface/Fournisseurs';
   styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
-  @Input() imageFile: ArrayBuffer;
-  @Input() fournisseurs: any;
-  @Input() extension: string = '';
-  @Input() searchTerm: string = '';
-  @Input() pathToImage: string = '';
-  @Input() imageEnBase64: string = '';
-  @Input() getSelectedCategory: string = '';
-  @Input() extensionsAutorised = ['jpg', 'jpeg', 'png', 'jfif'];
-  @Input() filteredFournisseurs: Fournisseur[] = [];
-  @Input() libelleValue: string = '';
-  @Input() libCode: string = '';
-  @Input() ref: string = 'REF-';
-  @Input() categories: any;
-  @Input() selectedFournisseurs: any[] = [];
-  @Input() num: number = 1;
-  @Input() categoryId: number;
-  @Input() prix: number = 0;
-  @Input() stock: number = 0;
-  @Input() getValue: any;
-  @Input() updateref: any;
-  @Input() onCategoryChange: any;
-  @Input() getAllFournisseur: any;
-  @Input() onInputChange: any;
-  @Input() ajouterArticle: any;
-  @Input() onFileChange: any;
-  @Input() selectFournisseur: any;
+  stock: number = 0;
+  categories: any;
+  imageFile: any = '';
+  categoryId: number;
+  num: number = 1;
+  selectedFournisseurs: number[] = [];
+  libelleValue: string = '';
+  libCode: string = '';
+  fournisseurs: any;
+  searchTerm: string = '';
+  filteredFournisseurs: Fournisseur[] = [];
+  selectedFournisseursName: string[] = [];
+  getSelectedCategory: string = '';
+  ref: string = 'REF-';
+  extension: string = '';
+  extensionsAutorised = ['jpg', 'jpeg', 'png', 'jfif'];
   @Input() chargerCategorie: any;
-  @Input() deselectFournisseur: any;
-  @Input() selectedFournisseursName: string[] = [];
+
   @Input() categoryservice: CategorieService;
+  @Output() info = new EventEmitter();
 
   selectedStyle = {
     background: 'gray',
@@ -51,5 +42,108 @@ export class FormComponent implements OnInit {
   constructor() {}
   ngOnInit(): void {
     this.chargerCategorie();
+    console.log(this.categoryservice);
+  }
+  getValue() {
+    this.libCode =
+      this.libelleValue[0] + this.libelleValue[1] + this.libelleValue[2];
+  }
+  dataToInsert = {
+    libelle: '',
+    reference: this.ref,
+    image: this.imageFile,
+    fournisseur: this.selectedFournisseurs,
+    categorie: 0,
+    prix: 0,
+    stock: 0,
+  };
+  onFileChange(event: any) {
+    console.log(this.libCode);
+    this.imageFile = event.target.files[0];
+
+    this.dataToInsert.image = this.imageFile;
+
+    this.extension = this.imageFile.name.split('.')[1];
+    if (!this.extensionsAutorised.includes(this.extension)) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Vous devez choisir une image',
+      });
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imageFile = reader.result as string;
+    };
+    reader.readAsDataURL(this.imageFile);
+  }
+  getAllFournisseur() {
+    this.categoryservice.getFournisseurs().subscribe((data) => {
+      this.fournisseurs = data;
+      console.log(this.fournisseurs);
+
+      const filteredFournisseurs = this.fournisseurs.filter((fournisseur) => {
+        return fournisseur.nom
+          .toLowerCase()
+          .includes(this.searchTerm.toLowerCase());
+      });
+      this.fournisseurs = filteredFournisseurs;
+      this.filteredFournisseurs = filteredFournisseurs;
+      if (this.searchTerm == '') {
+        this.fournisseurs = [];
+        this.filteredFournisseurs = [];
+      }
+      console.log(this.filteredFournisseurs);
+    });
+  }
+  selectFournisseur(fournisseur: any) {
+    this.selectedFournisseurs.push(fournisseur.id);
+    this.selectedFournisseursName.push(fournisseur.nom);
+    console.log(this.selectedFournisseurs);
+    fournisseur.selected = true;
+    this.searchTerm = this.selectedFournisseursName.map((f) => f).join(', ');
+  }
+  deselectFournisseur(fournisseur: any) {
+    this.selectedFournisseurs = this.selectedFournisseurs.filter(
+      (f) => f !== fournisseur.id
+    );
+    console.log(this.selectedFournisseurs);
+    fournisseur.selected = false;
+    this.selectedFournisseursName = this.selectedFournisseursName.filter(
+      (f) => f !== fournisseur.nom
+    );
+    this.searchTerm = this.selectedFournisseursName.map((f) => f).join(', ');
+  }
+  onInputChange() {
+    this.selectedFournisseurs = [];
+  }
+  updateref() {
+    const libCode = this.libCode.substring(0, 3);
+    let ref = 'REF-' + libCode.toUpperCase();
+    ref += '-' + this.getSelectedCategory.toUpperCase();
+    ref += '-' + this.num;
+    this.ref = ref;
+  }
+  onCategoryChange(event: any) {
+    this.categoryId = event.target.options.selectedIndex;
+  }
+  libelle = new FormControl('');
+  prixArticle = new FormControl('');
+  stockArticle = new FormControl('');
+  check(form: NgForm) {
+    console.log(this.ref);
+    
+    const newLibelle = this.libelle.value;
+    const newPrix = this.prixArticle.value;
+    const newStock = this.stockArticle.value;
+    this.dataToInsert.libelle = newLibelle;
+    this.dataToInsert.prix = +newPrix;
+    this.dataToInsert.stock = +newStock;
+    this.dataToInsert.reference = this.ref;
+    this.dataToInsert.fournisseur = this.selectedFournisseurs;
+    this.dataToInsert.image = this.imageFile;
+    this.dataToInsert.categorie = this.categoryId;
+    this.info.emit(this.dataToInsert);
   }
 }
